3541fee1021cb2aee2c4145e1df108cb0357116c
Leonid <79429232+LeonidYasin@users.noreply.github.com>
2025-08-14 21:35:49 +0300

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -3,11 +3,7 @@
 """
 
 from .price_calculator import PriceCalculator
-from .taxi_service import TaxiService
-from .delivery_service import DeliveryService
 
 __all__ = [
-    'PriceCalculator',
-    'TaxiService', 
-    'DeliveryService'
+    'PriceCalculator'
 ]
